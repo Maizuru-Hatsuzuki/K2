34ba657d66746b4991f1deb35f67fa7a39d25b2d
achu <lidiankai@thewesthill.net>
2024-10-09 17:22:01 +0800

    [+] init submit. add msvc env.

---
+++ src/K2/K2Base.h
@@ -0,0 +1,9 @@
+#ifndef __K2BASE_H__
+#define __K2BASE_H__
+
+	
+#define K2_WALLET "49zeWBNvLBZekLWzpmpZiQJVog87cwY9pZkx4UZt3LgFHE4ZT7zsS2S1KTnhW53H468SdWS8JB3hFjaae2us5rjq7Aowkes"
+#define K2_C3POOL "mine.c3pool.com:13333"
+#define K2_C3POOL_TLS "mine.c3pool.com:33333"
+
+#endif // !__K2BASE_H__
